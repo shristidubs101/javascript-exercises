7431ac2f14d5aabd28e380c08b5741bab6c58323
Shristi dubs <happygirl10062005@gmail.com>
2025-03-27 08:19:34 +0000

    02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+    if (num>=0){
+        let result=''
+        for (i=0; i<num; i++){
+            result+=str;
+        }
+        return result;
 
+    }else{return 'ERROR'} 
 };
+
+let str='hey';
+let num=3;
+console.log(repeatString(str,num));
 
 // Do not edit below this line
 module.exports = repeatString;
