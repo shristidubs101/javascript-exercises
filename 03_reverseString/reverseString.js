650fb407a23ad7ee708d51be47e11156d09397d9
Shristi dubs <happygirl10062005@gmail.com>
2025-03-29 15:12:49 +0000

    solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    
+    let result='';
+    for (i in str)
+    {
+        result= str[i] + result;
+    }
+    return result;
 };
+
+let str='hello'
+console.log(reverseString(str))
 
 // Do not edit below this line
 module.exports = reverseString;
