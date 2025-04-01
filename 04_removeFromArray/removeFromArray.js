064b52fa792ee60f5fccd8dab952cdaa44da7850
Shristi dubs <happygirl10062005@gmail.com>
2025-04-01 10:02:13 +0000

    solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    if (!Array.isArray(array)) return array;
+
+    const valuesToRemove= Array.prototype.slice.call(arguments,1);
+
+    for (let i = array.length-1; i>=0; i--){
+        if (valuesToRemove.includes(array[i])){
+            array.splice(i,1);
+        }
+    }
+    return array;
 };
+
+console.log(removeFromArray([1,2,3,4,5],3,4))
 
 // Do not edit below this line
 module.exports = removeFromArray;
+                    
