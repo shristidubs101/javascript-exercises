7ed2d25f11fa5df94035e0e9b77eaadcae839915
Shristi dubs <happygirl10062005@gmail.com>
2025-05-11 07:32:16 +0000

    08 exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,55 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let summ = 0;
+
+  if (array.length === 0) {
+    return 0;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    summ = summ + array[i];
+  }
+
+  return summ;
 };
 
-const multiply = function() {
 
+const multiply = function(array) {
+  let result = 1;
+
+  if (array.length === 0) {
+    return 0;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    result = result * array[i];
+  }
+
+  return result;
 };
 
-const power = function() {
-	
+
+const power = function(base, power) {
+  return (base ** power)	
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n < 0) return undefined; // Handle negative input
+
+  let fact = 1;
+  for (let i = 2; i <= n; i++) {
+    fact *= i;
+  }
+  return fact;
 };
+
 
 // Do not edit below this line
 module.exports = {
