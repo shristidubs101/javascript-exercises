b9554f0ee0a0134508baa3612d68a9c9a4f9a33f
Shristi dubs <happygirl10062005@gmail.com>
2025-05-12 05:31:21 +0000

    09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let revStr="";
+    let newStr = str.toLowerCase().replace(/[\s\W]/g, "");
+    
+    for(let i = newStr.length-1; i>=0; i--){
+        revStr = revStr + newStr[i];
+    }
 
+    return revStr === newStr;
 };
 
 // Do not edit below this line
