827ff07a985ca8a5104ce907d6e7458b5a29de4a
Shristi dubs <happygirl10062005@gmail.com>
2025-05-12 05:51:00 +0000

    10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    
+    if (num === 0){
+        return 0;
+    }
+
+    if (num < 0){
+        return "OOPS";
+    }
+
+    let a=0;
+    let b=1;
+
+    for (let i=1; i<num; i++){
+        let c= a+b;
+        a=b;
+        b=c;
+    }
+    return b;
 
 };
 
